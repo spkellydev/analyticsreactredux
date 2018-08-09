d08a3be8604e2de2167d6aa760f17b2c72fa1727
Sean Kelly <spkellydev@gmail.com>
2018-08-08 23:47:00 -0400

    change auth skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
+import * as actions from "actions";
 
-export default () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  renderButton() {
+    let msg;
+    if (this.props.auth) {
+      msg = "Sign Out";
+    } else {
+      msg = "Sign In";
+    }
+
+    return (
+      <button onClick={() => this.props.changeAuth(!this.props.auth)}>
+        {msg}
+      </button>
+    );
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  actions
+)(App);
