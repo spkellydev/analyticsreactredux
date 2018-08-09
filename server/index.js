655ec120c9aae39c11bffbf51eb2d70c46409675
Sean Kelly <seankelly@Seans-MacBook-Pro.local>
2018-08-09 14:01:21 -0400

    adding devdependency for concurrent server/client application

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require('path')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 const app = express()
 
-app.use(express.static(path.join(__dirname, "../build"))); //serves the index.html
+app.use(express.static(path.join(__dirname, "../public"))); //serves the index.html
 
+/**
+ * Named routes for Express must be declared before the React application
+ */
 app.get('/ga', (req, res) => {
     const getData = require('./lib/analytics')
 
@@ -14,11 +17,17 @@ app.get('/ga', (req, res) => {
     })
 })
 
+/**
+ * Serve the React application
+ * Use the wildcard so that routes defer to the React application
+ */
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'))
 })
 
-
+/**
+ * Serve the application
+ */
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
 
 module.exports = app
